adeab0a9fd87040eb520b5ae89e73637adef520d
Đoàn Trần Phi Khánh <80077540+khanhdoan09@users.noreply.github.com>
2023-03-04 21:47:11 +0700

    update cart with voucher

--- fe/src/pages/Cart/Cart.js
+++ fe/src/pages/Cart/Cart.js
@@ -1,22 +1,23 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 // import CartEmpty from './CartEmpty';
 import CartItem from './CartItem';
 import Slider from 'react-slick';
 import ProductSeller from '~/components/ProductSeller';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import Voucher from '~/components/Voucher';
 
 function Cart() {
+    const voucherRef = useRef();
     const [showModal, setShowModal] = useState(false);
     const [checkAll, setCheckAll] = useState(true);
+    const [showVouchers, setShowVouchers] = useState(false);
 
     function outOfStock() {
         return (
             <div>
                 <div>
-                    <div className="mb-3 flex items-center text-orange-400 text-xs">
-                        Sản phẩm đang tạm hết hàng
-                    </div>
+                    <div className="mb-3 flex items-center text-orange-400 text-xs">Sản phẩm đang tạm hết hàng</div>
                 </div>
             </div>
         );
@@ -31,13 +32,13 @@ function Cart() {
                     display: 'absolute',
                     backgroundColor: '#003366',
                     color: 'blue',
-                    left: '-2%',
+                    left: '0%',
                     borderRadius: '100%',
                     padding: 0,
                     margin: 0,
                     paddingTop: '1px',
                     paddingRight: '21px',
-                    zIndex: 5,
+                    zIndex: 1,
                 }}
                 onClick={onClick}
             />
@@ -53,7 +54,7 @@ function Cart() {
                     display: 'absolute',
                     backgroundColor: '#003366',
                     color: 'blue',
-                    right: '-2%',
+                    right: '0%',
                     borderRadius: '100%',
                     border: 'none',
                     paddingTop: '1px',
@@ -85,37 +86,63 @@ function Cart() {
     };
 
     return (
-        <div className="bg-[rgb(237,240,243)] py-5">
+        <div className="bg-[rgb(237,240,243)] py-5" id="num">
             <div className="w-full sm:flex flex-wrap justify-evenly max-sm:justify-start ">
                 {/* <CartEmpty></CartEmpty> */}
                 <div className="px-1 pb-3 rounded-xl">
+                    <div className="mb-5 sm:mr-3 px-3 flex items-center justify-between px-4 bg-contain bg-[url(https://firebasestorage.googleapis.com/v0/b/pharmacy-969d7.appspot.com/o/voucher%2Fbg-voucher.png?alt=media&token=24592f56-63b9-4e23-8006-a639ca5b8028)]">
+                        <div className="flex items-center">
+                            <img
+                                width={40}
+                                src="https://firebasestorage.googleapis.com/v0/b/pharmacy-969d7.appspot.com/o/voucher%2Fvoucher.png?alt=media&token=44249f28-5df6-435f-81a5-1f7544564e19"
+                            ></img>
+                            <p className="text-[#1250dc] text-[14px] flex flex-wrap">
+                                <span className="mr-1 font-semibold">Khuyến mại </span>
+                                <span>dành riêng cho bạn</span>
+                            </p>
+                        </div>
+                        <div
+                            className="flex items-center cursor-pointer"
+                            onClick={() => {
+                                document.body.style.overflow = 'hidden';
+
+                                setShowVouchers(true);
+                            }}
+                        >
+                            <span className="whitespace-nowrap text-[#1250dc] bg-[#eaeffa] text-[14px] font-[500] tracking-[.01em] rounded-full py-1 px-5">
+                                Chọn ngay
+                            </span>
+                            <span className="text-[#728091] max-sm:hidden">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-6 h-6"
+                                >
+                                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                                </svg>
+                            </span>
+                        </div>
+                    </div>
                     <div className="bg-white sm:mr-3 px-3 rounded-xl ">
                         <div>
                             <div className="flex items-center">
-                                <div className="flex text-sm my-3 w-80">
+                                <div className="flex text-sm my-3 w-80 font-[500]">
                                     <span
                                         onClick={() => setCheckAll(!checkAll)}
                                         className={`rounded-full p-1 mr-3 w-5 h-5 flex items-center justify-center text-white cursor-pointer ${
-                                            checkAll
-                                                ? 'bg-sky-700 border border-sky-700'
-                                                : 'border border-black'
+                                            checkAll ? 'bg-sky-700 border border-sky-700' : 'border border-black'
                                         }`}
                                     >
-                                        {checkAll ? (
-                                            <i className="fa-solid fa-check"></i>
-                                        ) : null}
+                                        {checkAll ? <i className="fa-solid fa-check"></i> : null}
                                     </span>
                                     Chọn tất cả (3)
                                 </div>
-                                <div className="text-sm w-32 text-center max-sm:hidden ">
-                                    Giá thành
-                                </div>
-                                <div className="text-sm w-20 text-center max-sm:hidden ">
-                                    Số lượng
-                                </div>
-                                <div className="text-sm w-32 text-center max-sm:hidden ">
-                                    Đơn vị
-                                </div>
+                                <div className="text-sm w-32 text-center max-sm:hidden font-[500]">Giá thành</div>
+                                <div className="text-sm w-20 text-center max-sm:hidden font-[500]">Số lượng</div>
+                                <div className="text-sm w-32 text-center max-sm:hidden font-[500]">Đơn vị</div>
                             </div>
                         </div>
                         <div>
@@ -126,7 +153,7 @@ function Cart() {
                         </div>
                     </div>
                     <div className="sm:w-[700px] my-3">
-                        <h2 className="font-semibold">Sản phẩm vừa xem</h2>
+                        <h2 className="font-[500] tracking-[.0025em] text-[#020b27] text-[18px]">Sản phẩm vừa xem</h2>
                         <Slider {...settings} className="m-0 p-0 relative">
                             {Array.from({ length: 6 }, (v, i) => (
                                 <div key={i} className="">
@@ -148,61 +175,37 @@ function Cart() {
                         </Slider>
                     </div>
                 </div>
-                <div className="sticky h-fit top-0 flex">
-                    <div className="z-10 w-72 border-2 bottom-0 bg-white h-fit py-4 px-3 sm:rounded-3xl max-sm:rounded-t-3xl max-sm:w-screen max-sm:fixed">
-                        <div
-                            className={`${
-                                showModal ? 'block' : 'hidden'
-                            } sm:block`}
-                        >
+                <div className="sticky h-fit top-0 flex z-20">
+                    <div className="z-10 w-96 border-2 bottom-0 bg-white h-fit py-4 px-3 sm:rounded-3xl max-sm:rounded-t-3xl max-sm:w-screen max-sm:fixed">
+                        <div className={`${showModal ? 'block' : 'hidden'} sm:block sm:text-[16px] max-sm:text-sm`}>
                             <div className="flex justify-between my-2">
-                                <h5 className="sm:text-[15px] max-sm:text-sm text-slate-600">
-                                    Tổng tiền
-                                </h5>
-                                <h5 className="sm:text-[15px] max-sm:text-sm">
-                                    824.400đ
-                                </h5>
+                                <h5 className="text-slate-600">Tổng tiền</h5>
+                                <h5>824.400đ</h5>
                             </div>
                             <div className="flex justify-between my-2">
-                                <h5 className="sm:text-[15px] max-sm:text-sm text-slate-600">
-                                    Giảm giá trực tiếp
-                                </h5>
-                                <h5 className="sm:text-[15px] max-sm:text-sm">
-                                    0đ
-                                </h5>
+                                <h5 className="text-slate-600">Giảm giá trực tiếp</h5>
+                                <h5>0đ</h5>
                             </div>
                             <div className="flex justify-between my-2">
-                                <h5 className="sm:text-[15px] max-sm:text-sm text-slate-600">
-                                    Giảm giá điểm thưởng
-                                </h5>
-                                <h5 className="sm:text-[15px] max-sm:text-sm">
-                                    824.400đ
-                                </h5>
+                                <h5 className="text-slate-600">Giảm giá voucher</h5>
+                                <h5>824.400đ</h5>
                             </div>
                             <div className="flex justify-between my-2">
-                                <h5 className="sm:text-[15px] max-sm:text-sm text-slate-600">
-                                    Tiết kiệm được
-                                </h5>
-                                <h5 className="sm:text-[15px] max-sm:text-sm">
-                                    0đ
-                                </h5>
+                                <h5 className="text-slate-600">Tiết kiệm được</h5>
+                                <h5>0đ</h5>
                             </div>
                         </div>
                         <div className="max-sm:flex justify-between items-center">
                             <div className="flex max-sm:flex-col justify-between sm:border-t mt-3 py-3">
-                                <h4 className="text-ms font-semibold">
-                                    Tạm tính
-                                </h4>
+                                <h4 className="text-[16px] font-semibold">Tạm tính</h4>
                                 <h4
-                                    className="text-base text-blue-700 font-bold flex items-center"
+                                    className="text-[20px] text-[#1250dc] font-[600] tracking-[.005em] flex items-center"
                                     onClick={() => setShowModal(!showModal)}
                                 >
                                     818.000đ
                                     <span
                                         className={`sm:hidden ml-2 duration-300 ${
-                                            showModal
-                                                ? 'rotate-180'
-                                                : 'rotate-0'
+                                            showModal ? 'rotate-180' : 'rotate-0'
                                         }`}
                                     >
                                         <svg
@@ -226,14 +229,64 @@ function Cart() {
                                 Đặt hàng (2)
                             </button>
                         </div>
-                        <div className="text-center mt-5 max-sm:hidden">
-                            <h6 className="text-xs text-slate-400">
-                                Nếu tiến hành đặt hàng, bạn đồng ý
-                            </h6>
-                            <h6 className="decoration-solid	text-xs font-semibold underline text-slate-600">
+                        <div className="text-center mt-4 max-sm:hidden">
+                            <h6 className="text-xs text-[#020b27] font-[400px]">Nếu tiến hành đặt hàng, bạn đồng ý</h6>
+                            <h6 className="mt-3 decoration-solid cursor-pointer text-xs font-semibold underline text-slate-600">
                                 Điều khoản của nhà thuốc
                             </h6>
                         </div>
+                    </div>
+                </div>
+            </div>
+            <div
+                className={`w-screen h-screen absolute bg-black opacity-50 top-0 z-40 ${
+                    showVouchers ? 'block' : 'hidden'
+                }`}
+            ></div>
+            <div
+                ref={voucherRef}
+                id="test"
+                className={`opacity-100	z-40 absolute right-0 top-0 bg-white h-full max-sm:w-full ${
+                    showVouchers ? 'block' : 'hidden'
+                }`}
+            >
+                <div className="opacity-100 flex justify-between items-center text-2xl font-[500] p-5">
+                    <p className="">Khuyến mại tặng bạn</p>
+                    <span
+                        className="cursor-pointer"
+                        onClick={() => {
+                            document.body.style.overflow = 'scroll';
+                            setShowVouchers(false);
+                        }}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-8 h-8"
+                        >
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </span>
+                </div>
+                <div className="bg-[#edf0f3] py-3 h-screen z-50 border-4">
+                    <p className="text-[#4a4f63] font-[600] px-5">Khuyến mại đơn hàng</p>
+                    <div className="h-3/4 overflow-auto px-5">
+                        <Voucher></Voucher>
+                        <Voucher></Voucher>
+                        <Voucher></Voucher>
+                        <Voucher></Voucher>
+                        <Voucher></Voucher>
+                        <Voucher></Voucher>
+                        <Voucher></Voucher>
+                        <Voucher></Voucher>
+                    </div>
+                    <div className="h-1/5 pb-10 w-full bg-white flex items-center rounded-2xl">
+                        <p className="py-3 mx-5 w-full rounded-3xl h-fit text-center bg-sky-600 text-white text-sm cursor-pointer ">
+                            Về giỏ hàng
+                        </p>
                     </div>
                 </div>
             </div>
